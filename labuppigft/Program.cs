29b1e86315e87142f8166631173648cf9d521b5e
fatima369jad <143130179+fatima369jad@users.noreply.github.com>
2023-09-10 13:39:27 +0200

    Add project files.

---
+++ labuppigft/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace labuppigft
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "367835215";
+            var SliceNumber = input[0];
+            var ranges= new List<string>();
+            var start = 0;
+             
+            for (var index=0;index < input.Length; index++)
+            {
+                var current = input[index];
+                if (! char.IsNumber(current))
+                    Console.WriteLine( "Invaild input! ");
+                if (current == SliceNumber && index != 0)
+                {
+                    ranges.Add(input[start..(index+1)]);
+                    if (index+1>=input.Length)
+                        break;
+                    SliceNumber= input[++index];
+                    start = index;
+
+
+
+                }
+            }
+            var total = 0;
+            foreach (var number in ranges)
+                total+=int.Parse(number);
+            Console.WriteLine(total);
+        }
+    }
+}
